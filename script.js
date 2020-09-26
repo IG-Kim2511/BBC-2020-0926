e0bccb0feb9d910707b5517719eae601996e0bb7
IG-Kim <iikim2511@gmail.com>
2020-09-26 12:12:38 -0400

    wip

--- script.js
+++ script.js
@@ -1,17 +1,96 @@
-"use strict";
+(
+  ()=>{
 
-/* part3 */
-document.write("part3-2 event");
-console.log(" part3-2 event");
 
-//  C 58 . JS 58
 
-// [Selectors]
+    // *js 02 : putting 'data-index' to .step , .graphic-item
 
-// [Event Listeners]
+    const stepElems=  document.querySelectorAll('.step');
 
-// [Funcitons]
-function name(event) {
-  // todo DIV
-  // Create LI
+    const graphicElems = document.querySelectorAll('.graphic-item');
+
+    let ioIndex;
+
+    const io = new IntersectionObserver((entries,observer)=>{
+      ioIndex = entries[0].target.dataset.index *1;
+      
+    });
+
+
+    for (let i = 0; i < stepElems.length; i++) {
+      io.observe(stepElems[i]);
+
+      stepElems[i].dataset.index = i;
+
+      graphicElems[i].dataset.index = i;
+      
+    }
+
+
+    // *js 06 better structure .visible
+
+    let currentItem = graphicElems[0];
+
+    // js 03-03
+
+    function active(action){
+      currentItem.classList.add('visible');
+      if (action) {
+        actions[action](true);
+        
+      }
+    }
+    function inactive(action){
+      currentItem.classList.add('visible');
+      if (action) {
+        actions[action](false);
+        
+      }
+    }
+
+    // *js 06 visible .scene-img
+
+    activate();
+
+    // *js 04 scroll and image .visible
+
+    window. addEventListener('scroll',()=>{
+      let step;
+      let boundingRect;
+
+
+// *js 02-31
+for (let i = 0; i < stepElems.length; i++) {
+
+  step= stepElems[i];
+
+  if(!step) continue;
+
+  boundingRect = step.getBoundingClientRect();
+
+  if(boundingRect.top > window.innerHeight *0.1 &&
+    boundingRect.top < window.innerHeight *0.8){
+
+      inactive();
+
+      currentItem = graphicElems[step.dataset.index];
+
+      active(currentItem.dataset.action);
+    
+  }
+
+  
 }
+//* js 0-21
+window.addEventListener('load',()=>{
+  setTimeout(() => {
+    scrollTo(0,0);
+  }, 100);
+
+});
+
+
+    });
+    active();
+
+  })();
